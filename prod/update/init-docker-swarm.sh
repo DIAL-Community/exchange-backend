abe0646be1931f3d0d2584d0f1eb5e8887f2ee8b
Steve Conrad <sconrad@digitalimpactalliance.org>
2019-07-19 15:06:15 -0500

    Wait for first instance before scaling to 2

--- prod/update/init-docker-swarm.sh
+++ prod/update/init-docker-swarm.sh
@@ -5,6 +5,7 @@ read -p "Please enter new version number: " version_number
 export T4D_VERSION=$version_number
 docker-compose -f ../../docker-compose.yml build
 docker-compose -f ../../docker-compose.yml push
-docker stack deploy --compose-file docker-compose.yml t4d-app
+docker stack deploy --compose-file ../../docker-compose.yml t4d-app
+sleep 60s
 
 docker service scale t4d-app_web=2
