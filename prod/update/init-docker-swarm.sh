bbc7f05a9b8f287feec770f610cf6f3d6fbd9b0c
Steve Conrad <sconrad@digitalimpactalliance.org>
2019-07-19 14:57:29 -0500

    Do initial deploy of docker stack

--- prod/update/init-docker-swarm.sh
+++ prod/update/init-docker-swarm.sh
@@ -1,6 +1,10 @@
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 docker swarm init
 
-./update-t4d-app.sh
+read -p "Please enter new version number: " version_number
+export T4D_VERSION=$version_number
+docker-compose -f ../../docker-compose.yml build
+docker-compose -f ../../docker-compose.yml push
+docker stack deploy --compose-file docker-compose.yml t4d-app
 
 docker service scale t4d-app_web=2
