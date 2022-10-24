44d63d73580839d44173c4f100725f29b1352ee0
Nikola Laskowska <nlaskowska@soldevelo.com>
2022-10-24 06:53:23 +0000

    CDS-1223: Mutation to update product indicators

---
+++ app/graphql/mutations/update_product_indicators.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'modules/maturity_sync'
+
+module Mutations
+  class UpdateProductIndicators < Mutations::BaseMutation
+    include Modules::MaturitySync
+
+    argument :indicators_data, [GraphQL::Types::JSON], required: true
+    argument :slug, String, required: true
+
+    field :product, Types::ProductType, null: true
+    field :errors, [String], null: true
+
+    def resolve(indicators_data:, slug:)
+      unless an_admin
+        return {
+          product: nil,
+          errors: ['Must be admin to update a product']
+        }
+      end
+
+      product = Product.find_by(slug: slug)
+
+      indicators_data.each do |indicator_data|
+        category_indicator = CategoryIndicator.find_by(slug: indicator_data['category_indicator_slug'])
+        product_indicator = ProductIndicator.find_by(category_indicator_id: category_indicator.id,
+                                                     product_id: product.id)
+        if product_indicator.nil?
+          product_indicator = ProductIndicator.new(category_indicator_id: category_indicator.id,
+                                                   product_id: product.id)
+        end
+
+        if indicator_data['value'].nil?
+          product_indicator.destroy!
+        else
+          product_indicator.indicator_value = indicator_data['value']
+          product_indicator.save!
+        end
+      end
+
+      if product.save
+        calculate_maturity_scores(product.id)
+        # Successful creation, return the created object with no errors
+        # We need to refetch product from DB to get the calculated maturity scores
+        {
+          product: Product.find_by(id: product.id),
+          errors: []
+        }
+      else
+        # Failed save, return the errors to the client
+        {
+          product: nil,
+          errors: product.errors.full_messages
+        }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -46,6 +46,7 @@ module Types
     field :update_product_projects, mutation: Mutations::UpdateProductProjects
     field :update_product_tags, mutation: Mutations::UpdateProductTags
     field :update_product_sdgs, mutation: Mutations::UpdateProductSdgs
+    field :update_product_indicators, mutation: Mutations::UpdateProductIndicators
 
     field :create_dataset, mutation: Mutations::CreateDataset
     field :update_dataset_countries, mutation: Mutations::UpdateDatasetCountries
