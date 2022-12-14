6bacaa0162a61f5c2d497213220ddb26f58b9788
Nikola Laskowska <nlaskowska@soldevelo.com>
2022-12-14 08:20:06 +0000

    CDS-1388: Add candidate datasets query

---
+++ app/graphql/queries/candidate_datasets_query.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Queries
+  class CandidateDatasetsQuery < Queries::BaseQuery
+    argument :search, String, required: false, default_value: ''
+    type [Types::CandidateDatasetType], null: true
+
+    def resolve(search:)
+      return nil if context[:current_user].nil? || !context[:current_user].roles.include?('admin')
+
+      candidate_datasets = CandidateDataset.order(:name)
+      candidate_datasets = candidate_datasets.name_contains(search) unless search.blank?
+      candidate_datasets
+    end
+  end
+end
