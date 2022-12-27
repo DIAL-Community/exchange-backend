60bff06b5c8a148bfe63ab186c71dba51f7739df
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2022-12-27 12:14:39 +0100

    CDS-1406: Updated apply as owner mutation to allow applying as dataset owner

---
+++ db/migrate/20221227105322_add_dataset_user_to_user_role.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+class AddDatasetUserToUserRole < ActiveRecord::Migration[6.1]
+  def up
+    execute(<<-ALTER_SQL)
+      ALTER TYPE user_role RENAME TO user_role_migrated;
+      CREATE TYPE user_role AS ENUM (
+        'admin',
+        'ict4sdg',
+        'principle',
+        'user',
+        'org_user',
+        'org_product_user',
+        'product_user',
+        'mni',
+        'content_writer',
+        'content_editor',
+        'dataset_user'
+        );
+      ALTER TABLE candidate_roles
+        ALTER COLUMN roles DROP DEFAULT,
+        ALTER COLUMN roles TYPE _user_role
+          USING roles::text::user_role[],
+        ALTER COLUMN roles SET DEFAULT '{}'::user_role[];
+      ALTER TABLE users
+        ALTER COLUMN roles DROP DEFAULT,
+        ALTER COLUMN roles TYPE _user_role
+          USING roles::text::user_role[],
+        ALTER COLUMN roles SET DEFAULT '{}'::user_role[];
+      ALTER TABLE users
+        ALTER COLUMN role DROP DEFAULT,
+        ALTER COLUMN role TYPE user_role
+          USING role::text::user_role,
+        ALTER COLUMN role SET DEFAULT 'user'::user_role;
+      DROP TYPE user_role_migrated;
+    ALTER_SQL
+  end
+
+  def down
+    execute(<<-ALTER_SQL)
+    ALTER TYPE user_role RENAME TO user_role_migrated;
+    CREATE TYPE user_role AS ENUM (
+      'admin',
+      'ict4sdg',
+      'principle',
+      'user',
+      'org_user',
+      'org_product_user',
+      'product_user',
+      'mni',
+      'content_writer',
+      'content_editor'
+      );
+    ALTER TABLE candidate_roles
+      ALTER COLUMN roles DROP DEFAULT,
+      ALTER COLUMN roles TYPE _user_role
+        USING roles::text::user_role[],
+      ALTER COLUMN roles SET DEFAULT '{}'::user_role[];
+    ALTER TABLE users
+      ALTER COLUMN roles DROP DEFAULT,
+      ALTER COLUMN roles TYPE _user_role
+        USING roles::text::user_role[],
+      ALTER COLUMN roles SET DEFAULT '{}'::user_role[];
+    ALTER TABLE users
+      ALTER COLUMN role DROP DEFAULT,
+      ALTER COLUMN role TYPE user_role
+        USING role::text::user_role,
+      ALTER COLUMN role SET DEFAULT 'user'::user_role;
+    DROP TYPE user_role_migrated;
+    ALTER_SQL
+  end
+end
