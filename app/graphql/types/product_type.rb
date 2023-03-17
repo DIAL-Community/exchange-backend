a4ef43b03954944836d518f0833760e70123e4c7
Steve Conrad <sconrad@digitalimpactalliance.org>
2023-03-17 16:11:07 -0500

    Provide resolver for is_linked_with_dpi

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -69,7 +69,7 @@ module Types
     field :pricing_details, String, null: true
     field :hosting_model, String, null: true
 
-    field :is_linked_with_dpi, Boolean, null: false
+    field :is_linked_with_dpi, Boolean, null: false, method: :is_linked_with_dpi
 
     field :playbooks, [Types::PlaybookType], null: true
   end
