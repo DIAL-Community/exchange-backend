8e2177612df2b0f4bcd20c684dc3d0444707552d
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-04-26 16:25:04 -0400

    Cleaning up error messages and removing unused code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'errors/not_found'
-  get 'errors/server_error'
   mount(GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql') if Rails.env.development?
 
   post '/graphql', to: 'graphql#execute'
@@ -10,9 +8,16 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-  resources :task_trackers
-
   post '/entities/process-file', to: 'entities#process_file'
+
+  root to: redirect(path: '/products')
+
+  resources :candidate_organizations do
+    member do
+      post 'reject'
+      post 'approve'
+    end
+  end
 
   resources :candidate_products do
     member do
@@ -21,76 +26,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :candidate_roles do
-    member do
-      post 'reject'
-      post 'approve'
-    end
-  end
-
-  resources :glossaries
-  resources :settings
-
-  resources :rubric_categories, only: %i[index update create destroy]
-  resources :category_indicators, only: %i[index update create destroy]
-  resources :maturity_rubrics do
-    resources :rubric_categories do
-      resources :category_indicators
-    end
-  end
-
-  resources :moves, only: %i[index update create destroy]
-  resources :activities, only: %i[index update create destroy]
-
-  resources :plays do
-    resources :moves
-  end
-
-  resources :playbooks do
-    post 'assign_play'
-    post 'remove_play'
-    resources :plays do
-      resources :moves
-    end
-  end
-
-  resources :handbook_pages, only: %i[index update create destroy]
-  resources :handbooks do
-    get 'count', on: :collection
-    post 'upload_design_images', on: :collection
-    member do
-      post 'convert_pages'
-    end
-    resources :handbook_pages do
-      member do
-        get 'copy_page'
-        get 'edit_content'
-        get 'load_design'
-        post 'save_design'
-        patch 'save_design'
-        get 'create_pdf'
-        get 'show_pdf'
-      end
-    end
-  end
-
-  resources :tags
-  resources :use_case_steps
-
-  resources :projects do
-    get 'count', on: :collection
-    get 'export_data', on: :collection
-    member do
-      post 'favorite_project'
-      post 'unfavorite_project'
-    end
-  end
-
-  get 'deploys/index'
-  get 'about/learnmore'
-  get 'about', to: 'about#index'
-
-  devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions', confirmations: 'confirmations' }
+  devise_for :users, controllers: {
+    confirmations: 'confirmations',
+    registrations: 'registrations',
+    sessions: 'sessions'
+  }
   scope '/devise' do
     resources :users do
       get 'statistics', on: :collection
@@ -109,69 +49,6 @@ Rails.application.routes.draw do
     post '/auth/reset-password', to: 'authentication#reset_password'
     post '/auth/apply-reset-token', to: 'authentication#apply_reset_token'
     post '/auth/validate-reset-token', to: 'authentication#validate_reset_token'
-  end
-
-  root to: redirect(path: '/products')
-
-  resources :covid, only: [:index]
-
-  resources :candidate_organizations do
-    member do
-      post 'reject'
-      post 'approve'
-    end
-  end
-
-  resources :portal_views do
-    member do
-      post 'select'
-    end
-  end
-
-  resources :products do
-    get 'count', on: :collection
-    get 'export_data', on: :collection
-    member do
-      post 'favorite_product'
-      post 'unfavorite_product'
-    end
-  end
-
-  resources :building_blocks do
-    get 'count', on: :collection
-    get 'export_data', on: :collection
-  end
-
-  resources :sustainable_development_goals, only: %i[index show] do
-    get 'count', on: :collection
-  end
-
-  resources :sdg_targets, only: %i[index show]
-
-  resources :use_case_steps, only: %i[new create edit update show]
-  resources :use_cases do
-    get 'count', on: :collection
-    get 'export_data', on: :collection
-    resources :use_case_steps, only: %i[new create edit update show]
-    member do
-      post 'favorite_use_case'
-      post 'unfavorite_use_case'
-    end
-  end
-
-  resources :workflows do
-    get 'count', on: :collection
-    get 'export_data', on: :collection
-  end
-
-  resources :deploys do
-    get 'show_messages'
-    post 'add_ssh_user'
-  end
-
-  resources :organizations do
-    get 'count', on: :collection
-    get 'export_data', on: :collection
   end
 
   # Start of external API routes
@@ -225,85 +102,15 @@ Rails.application.routes.draw do
 
   # End of external API routes
 
-  resources :operator_services
   resources :audits, only: [:index]
-  resources :stylesheets
-
-  resources :cities, only: %i[index show]
-  resources :countries
-
-  resources :locations do
-    resources :organizations
-  end
-
-  resources :sectors do
-    resources :organizations
-    resources :use_cases
-  end
-
-  resources :contacts do
-    resources :organizations
-  end
-
-  resources :tags
-  resources :use_case_steps
 
   get '/object_counts', to: 'application#object_counts', as: :object_counts
-  post '/add_filter', to: 'application#add_filter', as: :add_filter
-  post '/add_filters', to: 'application#add_filters', as: :add_filters
-  post '/remove_filter', to: 'application#remove_filter', as: :remove_filter
-  post '/remove_all_filters', to: 'application#remove_all_filters', as: :remove_all_filters
-  get '/fetch_filters', to: 'application#fetch_filters', as: :fetch_filters
-  get '/agg_capabilities', to: 'organizations#agg_capabilities', as: :agg_capabilities
-  get '/agg_services', to: 'organizations#agg_services', as: :agg_services
-  get '/service_capabilities', to: 'organizations#service_capabilities', as: :service_capabilities
-  get '/update_capability', to: 'organizations#update_capability', as: :update_capability
-  get '/privacy', to: 'about#privacy', as: :privacy
-  get '/terms', to: 'about#terms', as: :terms
   get '/healthcheck', to: 'about#healthcheck', as: :healthcheck
 
-  post '/save_url', to: 'application#save_url', as: :save_url
-  post '/remove_url', to: 'application#remove_url', as: :remove_url
   post '/send_email', to: 'application#send_email', as: :send_email
   post '/create_issue', to: 'application#create_issue', as: :create_issue
 
-  get 'export', to: 'organizations#export'
-  get 'candidate_organization_duplicates', to: 'candidate_organizations#duplicates'
-  get 'contact_duplicates', to: 'contacts#duplicates'
-  get 'location_duplicates', to: 'locations#duplicates'
-  get 'sector_duplicates', to: 'sectors#duplicates'
-  get 'product_duplicates', to: 'products#duplicates'
-  get 'building_block_duplicates', to: 'building_blocks#duplicates'
-  get 'organization_duplicates', to: 'organizations#duplicates'
-  get 'use_case_duplicates', to: 'use_cases#duplicates'
-  get 'workflow_duplicates', to: 'workflows#duplicates'
-  get 'glossary_duplicates', to: 'glossaries#duplicates'
-  get 'deploys_refresh_list', to: 'deploys#refresh_list'
-  get 'project_duplicates', to: 'projects#duplicates'
-  get 'portal_view_duplicates', to: 'portal_views#duplicates'
-  get 'use_case_step_duplicates', to: 'use_case_steps#duplicates'
-  get 'tag_duplicates', to: 'tags#duplicates'
-  get 'category_indicator_duplicates', to: 'category_indicators#duplicates'
-  get 'handbook_duplicates', to: 'handbooks#duplicates'
-  get 'handbook_page_duplicates', to: 'handbook_pages#duplicates'
-  get 'candidate_product_duplicates', to: 'candidate_products#duplicates'
-
   post '/froala_image/upload' => 'froala_images#upload'
-
-  get 'covidresources', to: 'covid#resources'
-  get 'productlist', to: 'products#productlist', as: :productlist
-  get 'productmap', to: 'products#map'
-
-  get 'map_projects', to: 'projects#map_projects'
-  get 'map_covid', to: 'projects#map_covid'
-
-  get 'map', to: 'organizations#map'
-  get 'map_osm', to: 'organizations#map_osm'
-  get 'map_fs', to: 'organizations#map_fs'
-  get 'map_aggregators_osm', to: 'organizations#map_aggregators_osm'
-  get 'map_projects_osm', to: 'projects#map_projects_osm'
-
-  get 'bb_fs', to: 'building_blocks#bb_fs'
 
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#server_error', via: :all
