51fa18b44ffd28f9eefc43658fd333b222540943
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-05-30 11:37:00 -0400

    Changing create resource for move.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -21,7 +21,7 @@ module Types
 
     field :create_move, mutation: Mutations::CreateMove
     field :auto_save_move, mutation: Mutations::CreateMove
-    field :create_resource, mutation: Mutations::CreateResource
+    field :create_move_resource, mutation: Mutations::CreateMoveResource
 
     field :create_user, mutation: Mutations::CreateUser
 
