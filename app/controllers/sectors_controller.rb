c1c9f5136ff10d4577117a9f4f01710bfb1c4bd1
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-06-26 16:50:04 -0400

    Changing URI.decode to CGI.escape.

--- app/controllers/sectors_controller.rb
+++ app/controllers/sectors_controller.rb
@@ -35,13 +35,13 @@ class SectorsController < ApplicationController
     if sectors.count > default_page_size * current_page
       query['page'] = current_page + 1
       uri.query = Rack::Utils.build_query(query)
-      results['next_page'] = URI.decode(uri.to_s)
+      results['next_page'] = CGI.escape(uri.to_s)
     end
 
     if current_page > 1
       query['page'] = current_page - 1
       uri.query = Rack::Utils.build_query(query)
-      results['previous_page'] = URI.decode(uri.to_s)
+      results['previous_page'] = CGI.escape(uri.to_s)
     end
 
     results['results'] = sectors
@@ -76,13 +76,13 @@ class SectorsController < ApplicationController
     if sectors.count > default_page_size * current_page
       query['page'] = current_page + 1
       uri.query = Rack::Utils.build_query(query)
-      results['next_page'] = URI.decode(uri.to_s)
+      results['next_page'] = CGI.escape(uri.to_s)
     end
 
     if current_page > 1
       query['page'] = current_page - 1
       uri.query = Rack::Utils.build_query(query)
-      results['previous_page'] = URI.decode(uri.to_s)
+      results['previous_page'] = CGI.escape(uri.to_s)
     end
 
     results['results'] = sectors
