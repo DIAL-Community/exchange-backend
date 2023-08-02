6d50a0d63cead9ec49c7b00ca81d2be6aefba005
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-08-02 09:05:54 -0400

    Fixing lint issue.

--- app/graphql/paginated/paginated_datasets.rb
+++ app/graphql/paginated/paginated_datasets.rb
@@ -12,14 +12,13 @@ module Paginated
       unless search.blank?
         name_filter = datasets.name_contains(search)
         desc_filter = datasets.left_joins(:dataset_descriptions)
-                                   .where('LOWER(dataset_descriptions.description) like LOWER(?)', "%#{search}%")
+                              .where('LOWER(dataset_descriptions.description) like LOWER(?)', "%#{search}%")
         datasets = datasets.where(id: (name_filter + desc_filter).uniq)
       end
 
       filtered_sectors = sectors.reject { |x| x.nil? || x.empty? }
       unless filtered_sectors.empty?
-        datasets = datasets.joins(:sectors)
-                                     .where(sectors: { id: filtered_sectors })
+        datasets = datasets.joins(:sectors).where(sectors: { id: filtered_sectors })
       end
 
       offset_params = offset_attributes.to_h
