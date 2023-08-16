59032efafc4b67be34ac38f5d1b1f79f8d0be657
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-08-15 23:02:49 -0400

    Fixing lint.

--- app/graphql/types/candidate_product_type.rb
+++ app/graphql/types/candidate_product_type.rb
@@ -9,7 +9,7 @@ module Types
     field :repository, String, null: false
     field :submitter_email, String, null: false
     field :description, String, null: true
-    
+
     field :created_at, GraphQL::Types::ISO8601Date, null: true
 
     field :commercial_product, Boolean, null: false
