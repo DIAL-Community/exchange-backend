ebdef3300c8dba2f49c1ab38594a0908449b52dd
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-05 12:16:45 -0400

    Cleanup wizard extended data.

--- app/graphql/queries/wizard_query.rb
+++ app/graphql/queries/wizard_query.rb
@@ -2,44 +2,11 @@
 
 module Queries
   class WizardQuery < Queries::BaseQuery
-    argument :sectors, [String], required: false, default_value: []
-    argument :use_case, String, required: false
-    argument :sdgs, [String], required: false
-    argument :building_blocks, [String], required: false
-
     type Types::WizardType, null: false
 
-    def resolve(sectors:, use_case:, sdgs:, building_blocks:)
+    def resolve
       wizard = {}
       wizard['digital_principles'] = DigitalPrinciple.all
-
-      curr_sectors = Sector.where(name: sectors, locale: I18n.locale)
-
-      curr_sdgs = SustainableDevelopmentGoal.where(name: sdgs)
-
-      if use_case == ''
-        sector_use_cases = []
-        sdg_use_cases = []
-
-        unless curr_sectors.nil?
-          sector_use_cases = UseCase.where(sector_id: curr_sectors, maturity: 'PUBLISHED')
-        end
-
-        curr_sdgs.each do |curr_sdg|
-          next if curr_sdgs.nil?
-          curr_targets = SdgTarget.where(sdg_number: curr_sdg.number)
-          sdg_use_cases << UseCase.where(
-            "id in (select use_case_id from use_cases_sdg_targets where sdg_target_id in (?)) and maturity='PUBLISHED'",
-            curr_targets.ids
-          )
-        end
-        wizard['use_cases'] = (sector_use_cases + sdg_use_cases.flatten).uniq
-      else
-        wizard['use_cases'] = [UseCase.find_by(name: use_case)]
-      end
-
-      wizard['building_blocks'] = BuildingBlock.where(name: building_blocks)
-      # Build list of resources manually
       wizard['resources'] = Resource.where(show_in_wizard: true)
       wizard
     end
