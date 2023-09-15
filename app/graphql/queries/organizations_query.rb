ee45cc847b1612ee6bc651e6f1ace5c2e9c883a9
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-15 12:48:38 -0400

    Updating for standalone endorser page.

--- app/graphql/queries/organizations_query.rb
+++ app/graphql/queries/organizations_query.rb
@@ -30,15 +30,20 @@ module Queries
     argument :search, String, required: false, default_value: ''
     argument :years, [Int], required: false, default_value: []
     argument :sectors, [String], required: false, default_value: []
+    argument :countries, [String], required: false, default_value: []
+    argument :endorser_only, Boolean, required: false, default_value: false
+    argument :endorser_level, String, required: false, default_value: ''
     argument :aggregators, [String], required: false, default_value: []
     argument :aggregator_only, Boolean, required: false, default_value: false
 
     type Types::OrganizationType.connection_type, null: false
 
-    def resolve(search:, years:, sectors:, aggregators:, aggregator_only:)
+    def resolve(search:, years:, sectors:, countries:, endorser_only:, endorser_level:, aggregators:, aggregator_only:)
       organizations = Organization.order(:name)
 
       organizations = organizations.where(is_mni: true) if aggregator_only
+      organizations = organizations.where(is_endorser: true) if endorser_only
+      organizations = organizations.where(endorser_level:) unless endorser_level.empty?
 
       filtered_aggregators = aggregators.reject { |x| x.nil? || x.empty? }
       organizations = organizations.where(id: filtered_aggregators) unless filtered_aggregators.empty?
@@ -55,6 +60,12 @@ module Queries
       unless filtered_sectors.empty?
         organizations = organizations.joins(:sectors)
                                      .where(sectors: { id: filtered_sectors })
+      end
+
+      filtered_countries = countries.reject { |x| x.nil? || x.empty? }
+      unless filtered_countries.empty?
+        organizations = organizations.joins(:countries)
+                                     .where(countries: { id: filtered_countries })
       end
 
       organizations = organizations.where('extract(year from when_endorsed) in (?)', years) unless years.empty?
