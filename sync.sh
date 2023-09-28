41ff56fa3655489251d5f03893a4863a4bf00b9a
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-28 14:09:34 -0400

    Prevent cascading failure on the nightly task.

--- sync.sh
+++ sync.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 set -e
 
-cd /product-evaluation-rubric
-git pull
+exit_code="Exits -> "
 
-cd /t4d
-rake sync:public_goods
-rake sync:digi_square_digital_good
-rake sync:osc_digital_good_local
-rake maturity_sync:update_license_data
-rake maturity_sync:update_statistics_data
-rake maturity_sync:update_code_review_indicators
-rake maturity_sync:update_language_data
-rake maturity_sync:update_products_languages
-rake maturity_sync:update_api_docs_indicators
-rake sync:fetch_website_data
-rake endorsers:sync_form_response
-rake data_processors:process_product_spreadsheet
-rake data_processors:process_dataset_spreadsheet
-rake data_processors:process_exported_json_files
-rake markdown_sync:use_case_definition
-rake opportunities_sync:sync_leverist
+cd /product-evaluation-rubric                       || exit_code+=" 1"
+git pull                                            || exit_code+=" 2"
+echo $exit_code
 
-rake opportunity:close_opportunities
+cd /t4d                                             || exit_code+=" 3"
+echo $exit_code
+
+rake sync:public_goods                              || exit_code+=" 4"
+rake sync:digi_square_digital_good                  || exit_code+=" 5"
+rake sync:osc_digital_good_local                    || exit_code+=" 6"
+rake maturity_sync:update_license_data              || exit_code+=" 7"
+rake maturity_sync:update_statistics_data           || exit_code+=" 8"
+rake maturity_sync:update_code_review_indicators    || exit_code+=" 9"
+rake maturity_sync:update_language_data             || exit_code+=" 10"
+rake maturity_sync:update_products_languages        || exit_code+=" 11"
+rake maturity_sync:update_api_docs_indicators       || exit_code+=" 12"
+rake sync:fetch_website_data                        || exit_code+=" 13"
+rake endorsers:sync_form_response                   || exit_code+=" 14"
+rake data_processors:process_product_spreadsheet    || exit_code+=" 15"
+rake data_processors:process_dataset_spreadsheet    || exit_code+=" 16"
+rake data_processors:process_exported_json_files    || exit_code+=" 17"
+rake markdown_sync:use_case_definition              || exit_code+=" 18"
+rake opportunities_sync:sync_leverist               || exit_code+=" 19"
+rake opportunity:close_opportunities                || exit_code+=" 20"
+echo $exit_code
