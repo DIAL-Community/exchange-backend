26a7ce26df3409fc2b5e8d517e6f31fbc53292ce
Steve Conrad <sconrad@digitalimpactalliance.org>
2023-11-17 19:47:52 +0000

    Apartment gem

---
+++ lib/modules/exchange.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+require 'apartment/elevators/generic'
+
+module Apartment
+  module Elevators
+    #   Provides a rack based tenant switching solution based on subdomains
+    #   Assumes that tenant name should match subdomain
+    #
+    class Exchange < Generic
+      # @return {String} - The tenant to switch to
+      def parse_tenant_name(request)
+        # request is an instance of Rack::Request
+
+        curr_host = URI.parse(request.referrer).host unless request.referrer.nil?
+        # The following is for the next-auth pages, which do not send origin or referer headers.
+        # We add the tenant manually before making the call
+        curr_host = request.env["HTTP_X_TENANT_ID"].split(':')[0] if request.referrer.nil? &&
+          !request.env["HTTP_X_TENANT_ID"].nil?
+        requested_tenant = ExchangeTenant.where(domain: curr_host).first
+        requested_tenant.nil? ? tenant_name = 'public' : tenant_name = requested_tenant.tenant_name
+        tenant_name
+      end
+    end
+  end
+end
---
+++ spec/factories/tenants.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+FactoryBot.define do
+  factory :tenant do
+    tenant_name { "MyString" }
+  end
+end
