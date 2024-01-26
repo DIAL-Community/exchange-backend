8f6ade05db05fcea96ee94f8113f89866e1a10f5
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-01-26 13:57:13 -0500

    Adding searching for authors.

--- app/graphql/types/resource_type.rb
+++ app/graphql/types/resource_type.rb
@@ -4,7 +4,8 @@ module Types
   class AuthorType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
-    field :email, String, null: false
+    field :slug, String, null: false
+    field :email, String, null: true
     field :picture, String, null: false
   end
 
