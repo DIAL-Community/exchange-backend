74cb7c104eae6093536b8a9be973f9d827293ec3
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-05-08 16:49:13 -0400

    Adding filtering for the dpi admin users.

--- app/graphql/paginated/paginated_users.rb
+++ app/graphql/paginated/paginated_users.rb
@@ -3,10 +3,13 @@
 module Paginated
   class PaginatedUsers < Queries::BaseQuery
     argument :search, String, required: false, default_value: ''
+    argument :roles, [String], required: false, default_value: []
+
     argument :offset_attributes, Attributes::OffsetAttributes, required: true
+
     type [Types::UserType], null: false
 
-    def resolve(search:, offset_attributes:)
+    def resolve(search:, roles:, offset_attributes:)
       if !unsecure_read_allowed && context[:current_user].nil?
         return []
       end
@@ -16,6 +19,11 @@ module Paginated
       users = User.order(:email)
       unless search.blank?
         users = users.name_contains(search)
+      end
+
+      filtered_roles = roles.reject { |x| x.nil? || x.empty? }
+      unless filtered_roles.empty?
+        users = users.where('roles && ARRAY[?]::user_role[]', filtered_roles)
       end
 
       offset_params = offset_attributes.to_h
--- app/graphql/paginated/pagination_attribute_user.rb
+++ app/graphql/paginated/pagination_attribute_user.rb
@@ -3,9 +3,11 @@
 module Paginated
   class PaginationAttributeUser < Queries::BaseQuery
     argument :search, String, required: false, default_value: ''
+    argument :roles, [String], required: false, default_value: []
+
     type Attributes::PaginationAttributes, null: false
 
-    def resolve(search:)
+    def resolve(search:, roles:)
       if !unsecure_read_allowed && context[:current_user].nil?
         return { total_count: 0 }
       end
@@ -15,6 +17,11 @@ module Paginated
       users = User.order(:name)
       unless search.blank?
         users = users.name_contains(search)
+      end
+
+      filtered_roles = roles.reject { |x| x.nil? || x.empty? }
+      unless filtered_roles.empty?
+        users = users.where('roles && ARRAY[?]::user_role[]', filtered_roles)
       end
 
       { total_count: users.count }
