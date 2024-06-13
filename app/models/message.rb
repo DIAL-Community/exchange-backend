713add838feed49bee70a5c0c9208bd9edb1721e
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-06-13 15:23:30 -0400

    Fixes for the ADLI post demo.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,20 @@
 
 class Message < ApplicationRecord
   belongs_to :created_by, class_name: 'User'
+
+  after_create :trigger_message_notification
+
+  def trigger_message_notification
+    contacts = Contact.where(source: DPI_TENANT_NAME)
+    contacts.each do |contact|
+      MessageMailer
+        .with(
+          current_user: User.find_by(email: contact.email),
+          current_contact: contact,
+          current_message: self
+        )
+        .message_action_notification
+        .deliver_now
+    end
+  end
 end
