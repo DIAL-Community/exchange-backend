e7c367f2e0c86c2e9c431a91cecf4b2816d61ee7
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-08-09 11:07:42 -0400

    Adding references field to record documents used by the chatbot to create answer.

--- app/graphql/types/chatbot_conversation_type.rb
+++ app/graphql/types/chatbot_conversation_type.rb
@@ -9,6 +9,23 @@ module Types
 
     field :chatbot_question, String, null: false
     field :chatbot_answer, String, null: false
-    field :chatbot_response, GraphQL::Types::JSON, null: true
+
+    field :chatbot_references, [String], null: true do
+      argument :first, Integer, required: false
+    end
+
+    def chatbot_references(reference_context)
+      chatbot_response = object.chatbot_response
+      chatbot_references = []
+
+      counter = 0
+      chatbot_response['output']['context'].each do |context|
+        break if counter >= reference_context[:first]
+        context_metadata = context['metadata']
+        chatbot_references << context_metadata['source'] unless context_metadata['source'].nil?
+        counter += 1
+      end
+      chatbot_references
+    end
   end
 end
