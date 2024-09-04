5c9c684f1b3be0bc52845cdd04c96d514d265cce
lukruz <lruzicki@soldevelo.com>
2024-09-04 10:19:08 +0000

    CDS-2012: added mutation and test for simple editing of extra attributes

--- db/structure.sql
+++ db/structure.sql
@@ -3662,7 +3662,7 @@ CREATE TABLE fao.products (
     pricing_url character varying,
     languages jsonb,
     gov_stack_entity boolean DEFAULT false NOT NULL,
-    extra_attributes jsonb DEFAULT '{}'::jsonb,
+    extra_attributes jsonb DEFAULT '[]'::jsonb,
     product_stage character varying
 );
 
@@ -7886,7 +7886,7 @@ CREATE TABLE health.products (
     pricing_url character varying,
     languages jsonb,
     gov_stack_entity boolean DEFAULT false NOT NULL,
-    extra_attributes jsonb DEFAULT '{}'::jsonb,
+    extra_attributes jsonb DEFAULT '[]'::jsonb,
     product_stage character varying
 );
 
@@ -12110,7 +12110,7 @@ CREATE TABLE public.products (
     pricing_url character varying,
     languages jsonb,
     gov_stack_entity boolean DEFAULT false NOT NULL,
-    extra_attributes jsonb DEFAULT '{}'::jsonb,
+    extra_attributes jsonb DEFAULT '[]'::jsonb,
     product_stage character varying
 );
 
@@ -27215,6 +27215,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20240712135449'),
 ('20240721194811'),
 ('20240806130712'),
-('20240814120047');
+('20240814120047'),
+('20240830132609');
 
 
