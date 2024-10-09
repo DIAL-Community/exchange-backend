7835d4677c81e11892856307047387e047b91bfb
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-10-09 09:16:43 -0400

    Updating comment object types.

--- app/graphql/types/candidate_status_type.rb
+++ app/graphql/types/candidate_status_type.rb
@@ -11,5 +11,6 @@ module Types
     field :terminal_status, Boolean, null: false
 
     field :next_candidate_statuses, [Types::CandidateStatusType], null: true
+    field :previous_candidate_statuses, [Types::CandidateStatusType], null: true
   end
 end
--- app/models/candidate_status.rb
+++ app/models/candidate_status.rb
@@ -4,13 +4,23 @@ class CandidateStatus < ApplicationRecord
   scope :name_contains, ->(name) { where('LOWER(candidate_statuses.name) like LOWER(?)', "%#{name}%") }
   scope :slug_starts_with, ->(slug) { where('LOWER(candidate_statuses.slug) like LOWER(?)', "#{slug}%\\_") }
 
-  has_many :candidate_status_relationships,
-    foreign_key: :current_candidate_status_id,
+  has_many :next_candidate_status_relationships,
+    join_table: 'candidate_status_relationships',
+    foreign_key: 'current_candidate_status_id',
     class_name: 'CandidateStatusRelationship'
 
   has_many :next_candidate_statuses,
-    through: :candidate_status_relationships,
+    through: :next_candidate_status_relationships,
     source: :next_candidate_status
+
+  has_many :previous_candidate_status_relationships,
+    join_table: 'candidate_status_relationships',
+    foreign_key: 'next_candidate_status_id',
+    class_name: 'CandidateStatusRelationship'
+
+  has_many :previous_candidate_statuses,
+    through: :previous_candidate_status_relationships,
+    source: :current_candidate_status
 
   # overridden
   def generate_slug
