c45b32b0f901a691644f15149a5dd018b162a65f
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2025-02-21 08:54:47 -0500

    Merging page configuration mutations.

--- app/graphql/mutations/update_site_setting_item_settings.rb
+++ app/graphql/mutations/update_site_setting_item_settings.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 module Mutations
-  class UpdateSiteSettingItemLayouts < Mutations::BaseMutation
+  class UpdateSiteSettingItemSettings < Mutations::BaseMutation
     argument :item_layouts, GraphQL::Types::JSON, required: true, default_value: {}
+    argument :item_configurations, GraphQL::Types::JSON, required: true, default_value: []
 
     field :site_setting, Types::SiteSettingType, null: true
     field :errors, [String], null: true
 
-    def resolve(item_layouts:)
+    def resolve(item_layouts:, item_configurations:)
       site_setting = SiteSetting.find_by(default_setting: true)
       site_setting_policy = Pundit.policy(context[:current_user], site_setting || SiteSetting.new)
       if site_setting.nil? || !site_setting_policy.edit_allowed?
@@ -18,6 +19,7 @@ module Mutations
       end
 
       site_setting.item_layouts = { layouts: item_layouts }
+      site_setting.item_configurations = { items: item_configurations }
       if site_setting.save
         # Successful creation, return the created object with no errors
         {
