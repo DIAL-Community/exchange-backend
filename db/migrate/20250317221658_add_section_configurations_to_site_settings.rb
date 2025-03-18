d957f5541c3d506aaad5edc6d5a0fbae8da5689a
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2025-03-17 23:50:44 -0400

    Updating default value for the section configurations.

--- db/migrate/20250317221658_add_section_configurations_to_site_settings.rb
+++ db/migrate/20250317221658_add_section_configurations_to_site_settings.rb
@@ -2,6 +2,6 @@
 
 class AddSectionConfigurationsToSiteSettings < ActiveRecord::Migration[7.1]
   def change
-    add_column(:site_settings, :section_configurations, :jsonb, null: false, default: [])
+    add_column(:site_settings, :section_configurations, :jsonb, null: false, default: {})
   end
 end
