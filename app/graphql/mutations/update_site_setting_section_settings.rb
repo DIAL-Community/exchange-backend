4d95d9616784170baaa7dcd3b180403fe8484787
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2025-03-19 20:12:33 +0000

    Configurable sections of the detail page

---
+++ app/graphql/mutations/update_site_setting_section_settings.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Mutations
+  class UpdateSiteSettingSectionSettings < Mutations::BaseMutation
+    argument :section_configurations, GraphQL::Types::JSON, required: true, default_value: []
+
+    field :site_setting, Types::SiteSettingType, null: true
+    field :errors, [String], null: true
+
+    def resolve(section_configurations:)
+      site_setting = SiteSetting.find_by(default_setting: true)
+      site_setting_policy = Pundit.policy(context[:current_user], site_setting || SiteSetting.new)
+      if site_setting.nil? || !site_setting_policy.edit_allowed?
+        return {
+          site_setting: nil,
+          errors: ['Editing site setting is not allowed.']
+        }
+      end
+
+      site_setting.section_configurations = section_configurations
+      if site_setting.save
+        # Successful creation, return the created object with no errors
+        {
+          site_setting:,
+          errors: []
+        }
+      else
+        # Failed save, return the errors to the client
+        {
+          site_setting: nil,
+          errors: site_setting.errors.full_messages
+        }
+      end
+    end
+  end
+end
---
+++ db/migrate/20250317221658_add_section_configurations_to_site_settings.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddSectionConfigurationsToSiteSettings < ActiveRecord::Migration[7.1]
+  def change
+    add_column(:site_settings, :section_configurations, :jsonb, null: false, default: {})
+  end
+end
