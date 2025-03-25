5e36a5bc4e997d07caaf4d7454b37096bf8cbae8
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2025-03-25 07:53:26 -0400

    Updating adli pages.

--- app/graphql/queries/contact_query.rb
+++ app/graphql/queries/contact_query.rb
@@ -57,13 +57,26 @@ module Queries
 
   class HubContactsQuery < Queries::BaseQuery
     argument :search, String, required: false, default_value: ''
+    argument :alumni, Boolean, required: false, default_value: false
+
     type [Types::ContactType], null: false
 
-    def resolve(search:)
+    def resolve(search:, alumni:)
       validate_access_to_resource(Contact.new)
       contacts = Contact.order(:name)
       contacts = contacts.name_contains(search) unless search.blank?
-      contacts.where(source: DPI_TENANT_NAME)
+      contacts = contacts.where(source: DPI_TENANT_NAME)
+      contacts.select do |contact|
+        consent, _ = contact.extra_attributes.select { |e| e['name'] == 'consent' }
+        puts "Consent: #{consent.inspect}"
+
+        adli_years = contact.extra_attributes.select { |e| e['name'] == 'adli-years' }
+        if alumni
+          adli_years.any? { |year| year.select { |y| y.to_i < Date.current.year } }
+        else
+          adli_years.any? { |year| year['value'].include?(Date.current.year) }
+        end
+      end
     end
   end
 end
