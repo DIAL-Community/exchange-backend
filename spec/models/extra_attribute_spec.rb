f191584c6a7ac790e8027bf54647ce0fe0f4ce00
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2025-04-14 18:31:17 -0400

    Fixing lint issue with the new components.

--- spec/models/extra_attribute_spec.rb
+++ spec/models/extra_attribute_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe ExtraAttribute, type: :model do
+RSpec.describe(ExtraAttribute, type: :model) do
   pending "add some examples to (or delete) #{__FILE__}"
 end
