d5202c1459e4b34f134f5ff6ea36a1e3defcd190
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2025-04-22 12:48:54 -0400

    Updating method to save polls from the lexical framework.

--- app/graphql/mutations/update_description_entity.rb
+++ app/graphql/mutations/update_description_entity.rb
@@ -1,15 +1,36 @@
 # frozen_string_literal: true
 
 module Mutations
-  class UpdatePlayDescription < Mutations::BaseMutation
+  class UpdateDescriptionEntity < Mutations::BaseMutation
     argument :slug, String, required: true
+    argument :type, String, required: true
     argument :owner, String, required: true
+    argument :field_name, String, required: true
+    argument :parent_slug, String, required: false
     argument :description, String, required: true
 
-    field :play, Types::PlayType, null: true
     field :errors, [String], null: true
 
-    def resolve(slug:, owner:, description:)
+    def resolve_description_entity(slug, type, owner, parent_slug)
+      case type
+      when 'PLAY'
+        play = Play.find_by(slug:, owned_by: owner)
+        return nil if play.nil?
+        PlayDescription.find_by(play:, locale: I18n.locale)
+      when 'MOVE'
+        play = Play.find_by(slug: parent_slug, owned_by: owner)
+        return nil if play.nil?
+        play_move = PlayMove.find_by(play:, slug: slug)
+        return nil if play_move.nil?
+        PlayMoveDescription.find_by(play_move_id: play_move, locale: I18n.locale)
+      when 'PLAYBOOK'
+        playbook = Playbook.find_by(slug:, owned_by: owner)
+        return nil if playbook.nil?
+        PlaybookDescription.find_by(playbook:, locale: I18n.locale)
+      end
+    end
+
+    def resolve(slug:, type:, owner:, description:, field_name:, parent_slug:)
       if context[:current_user].nil?
         return {
           play: nil,
@@ -17,46 +38,40 @@ module Mutations
         }
       end
 
-      play = Play.find_by(slug:, owned_by: owner)
-      if play.nil?
+      description_entity = resolve_description_entity(slug, type, owner, parent_slug)
+      if description_entity.nil?
         return {
-          play: nil,
-          errors: ['Unable to find play.']
+          errors: ['Unable to resolve description entity.']
         }
       end
 
-      play_description = PlayDescription.find_by(play:, locale: I18n.locale)
-      if play_description.description == description
+      if description_entity[field_name] == description
         # Don't try to save if the description is the same.
         return {
-          play:,
           errors: []
         }
       end
 
       updated_description = Nokogiri::HTML.fragment(description)
-      existing_description = Nokogiri::HTML.fragment(play_description.description)
+      existing_description = Nokogiri::HTML.fragment(description_entity[field_name])
       validate_equality(updated_description, existing_description)
-
-      play_description.description = description
 
       successful_operation = false
       ActiveRecord::Base.transaction do
-        assign_auditable_user(play_description)
-        play_description.save!
+        assign_auditable_user(description_entity)
+        description_entity[field_name] = description
+        description_entity.save!
         successful_operation = true
       end
 
       if successful_operation
         # Successful creation, return the created object with no errors
         {
-          play:,
           errors: []
         }
       else
         # Failed save, return the errors to the client
         {
-          play: nil,
           errors: play.errors.full_messages
         }
       end
